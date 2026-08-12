a6f9d15d9fae5fb8b0a1fb1a6299c580129e2f3b
Amit Sarker <amitireland2028@gmail.com>
2026-08-12 06:26:21 +0000

    Add Docker image layer caching lab

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.json([
+        {
+            name: 'Bob',
+            email: 'bob@gmail.com'
+        },
+        {
+            name: 'Alice',
+            email: 'alice@hotmail.com'
+        },
+	{
+	    name: 'Jake',
+	    email: 'jake@yahoo.com'
+        },
+	{
+            name: 'Amit',
+	    email: "amit@example.com"
+	}
+    ]);
+});
+
+app.listen(port, () => {
+    console.log(`Application listening on port ${port}`);
+});
